2d3faea7afdb50f4c1a38dffce3ad6b6fc9e33a0
Lucas <lucasgoncalves08@live.com>
2024-02-10 11:52:27 -0300

    create file to store technology path

---
+++ src/app/pages/technology/app-technology.routes.ts
@@ -0,0 +1,8 @@
+import { Routes } from "@angular/router";
+import { TechnologyComponent } from "./technology.component";
+
+export const appTechnologyRoutes: Routes = [
+  {
+    path: '', component: TechnologyComponent
+  }
+];
