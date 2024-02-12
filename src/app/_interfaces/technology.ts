e917fd333844be25bbdbd3f6ea13f9bf9298b87a
Lucas <lucasgoncalves08@live.com>
2024-02-12 13:52:48 -0300

    finishing technology component

---
+++ src/app/_interfaces/technology.ts
@@ -0,0 +1,10 @@
+export interface Technology {
+  images: {
+    mobile: string,
+    tablet: string,
+    desktop: string
+  };
+  name: string;
+  description: string;
+  active: boolean
+}
--- src/app/pages/technology/technology.component.ts
+++ src/app/pages/technology/technology.component.ts
@@ -1,13 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { Technology } from 'src/app/_interfaces/technology';
 
 @Component({
   selector: 'app-technology',
   templateUrl: './technology.component.html',
   styleUrls: ['./technology.component.css']
 })
-export class TechnologyComponent {
+export class TechnologyComponent implements OnInit{
 
-  technologies = [
+  currentTechnology: Technology = {
+    images: {
+      mobile: '',
+      tablet: '',
+      desktop: ''
+    },
+    name: '',
+    description: '',
+    active: false
+  };
+
+  technologies: Technology[] = [
     {
       name: "Launch vehicle",
       images: {
@@ -15,7 +27,8 @@ export class TechnologyComponent {
         tablet: "../../../assets/technology/image-launch-vehicle-tablet.svg",
         desktop: "../../../assets/technology/image-launch-vehicle-desktop.svg"
       },
-      description: "A launch vehicle or carrier rocket is a rocket-propelled vehicle used to carry a payload from Earth's surface to space, usually to Earth orbit or beyond. Our WEB-X carrier rocket is the most powerful in operation. Standing 150 metres tall, it's quite an awe-inspiring sight on the launch pad!"
+      description: "A launch vehicle or carrier rocket is a rocket-propelled vehicle used to carry a payload from Earth's surface to space, usually to Earth orbit or beyond. Our WEB-X carrier rocket is the most powerful in operation. Standing 150 metres tall, it's quite an awe-inspiring sight on the launch pad!",
+      active: false
     },
     {
       name: "Spaceport",
@@ -24,7 +37,8 @@ export class TechnologyComponent {
         tablet: "../../../assets/technology/image-spaceport-tablet.svg",
         desktop: "../../../assets/technology/image-spaceport-desktop.svg"
       },
-      description: "A spaceport or cosmodrome is a site for launching (or receiving) spacecraft, by analogy to the seaport for ships or airport for aircraft. Based in the famous Cape Canaveral, our spaceport is ideally situated to take advantage of the Earth’s rotation for launch."
+      description: "A spaceport or cosmodrome is a site for launching (or receiving) spacecraft, by analogy to the seaport for ships or airport for aircraft. Based in the famous Cape Canaveral, our spaceport is ideally situated to take advantage of the Earth’s rotation for launch.",
+      active: false
     },
     {
       name: "Space capsule",
@@ -33,7 +47,39 @@ export class TechnologyComponent {
         tablet: "../../../assets/technology/image-space-capsule-tablet.svg",
         desktop: "../../../assets/technology/image-space-capsule-desktop.svg"
       },
-      description: "A space capsule is an often-crewed spacecraft that uses a blunt-body reentry capsule to reenter the Earth's atmosphere without wings. Our capsule is where you'll spend your time during the flight. It includes a space gym, cinema, and plenty of other activities to keep you entertained."
+      description: "A space capsule is an often-crewed spacecraft that uses a blunt-body reentry capsule to reenter the Earth's atmosphere without wings. Our capsule is where you'll spend your time during the flight. It includes a space gym, cinema, and plenty of other activities to keep you entertained.",
+      active: false
     }
   ]
+
+  currentIndex = 0;
+
+  ngOnInit(): void {
+    this.setCurrentTechnology(this.currentIndex);
+  }
+
+  @ViewChild('technologyImg') technologyImg!: ElementRef<HTMLImageElement>;
+  @ViewChild('technologyInfoArticle') technologyInfoArticle!: ElementRef<HTMLElement>;
+
+  changeTechnology(index: number): void{
+    if(this.currentIndex === index) return;
+    this.currentIndex = index;
+    this.setCurrentTechnology(this.currentIndex);
+    this.toggleClasses();
+  }
+
+  private toggleClasses(): void{
+    this.technologyImg.nativeElement.classList.add('active');
+    this.technologyInfoArticle.nativeElement.classList.add('active');
+    setTimeout(() => {
+      this.technologyImg.nativeElement.classList.remove('active');
+      this.technologyInfoArticle.nativeElement.classList.remove('active');
+    }, 500)
+  };
+
+  private setCurrentTechnology(index: number): void{
+    this.currentTechnology.active = false;
+    this.currentTechnology = this.technologies[index];
+    this.currentTechnology.active = true;
+  }
 }
